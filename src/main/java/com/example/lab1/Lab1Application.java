0355e184fb19a39873daf7dfc374083f45f9b2ed
nadhirahh <nadhirahh97@gmail.com>
2023-05-14 17:30:31 +0800

    update first commit

--- src/main/java/com/example/lab1/Lab1Application.java
+++ src/main/java/com/example/lab1/Lab1Application.java
@@ -1,13 +1,25 @@
 package com.example.lab1;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Lab1Application {
+public class Lab1Application implements CommandLineRunner {
+	
+	public static final org.slf4j.Logger logger = LoggerFactory.getLogger(Lab1Application.class); 
 
 	public static void main(String[] args) {
+		logger.info("Its a continuous integration");
 		SpringApplication.run(Lab1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("Its my second logger");
+		
 	}
 
 }
--- src/test/java/com/example/lab1/Lab1ApplicationTests.java
+++ src/test/java/com/example/lab1/Lab1ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.lab1;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Lab1ApplicationTests {
+	
+	public static final org.slf4j.Logger logger = LoggerFactory.getLogger(Lab1Application.class); 
 
 	@Test
 	void contextLoads() {
+		logger.info("Its a test class");
+		assertEquals(true, true);
 	}
 
 }
